8dbfb8ebc069b5a259aad87f7b6c68e08bb8dbd6
DivyamPal <85020103+DivyamPal@users.noreply.github.com>
2021-08-19 23:41:51 +0530

    Adding files

---
+++ day24/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//  import App from "./app";
+// import App from "./Appday17";
+// import App from "./Appday18";
+// import App from"./Appday19";
+// import App from "./Appday20";
+// import App from "./Appday21";
+// import App from "./Appday22";
+import App from "./Appday24"
+
+ReactDOM.render(
+  <App></App>,
+  document.getElementById("root")
+);
