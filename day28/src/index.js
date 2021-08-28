b07ab14139df6c7fd2051c829215a41878058673
DivyamPal <85020103+DivyamPal@users.noreply.github.com>
2021-08-28 21:28:49 +0530

    Adding files

---
+++ day28/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+// import store from "./redux/store";
+
+//  import App from "./app";
+// import App from "./Appday17";
+// import App from "./Appday18";
+// import App from"./Appday19";
+// import App from "./Appday20";
+// import App from "./Appday21";
+// import App from "./Appday22";
+// import App from "./Appday24"
+// import App from "./Eco"
+// import App from "./Appanime"
+// import store from "./store"
+// import App from "./Appday26"
+import App from "./Appday28"
+
+ReactDOM.render(
+  <App></App>,
+  document.getElementById("root")
+);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//       <Provider store={store}>
+//           <App />
+//       </Provider>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
