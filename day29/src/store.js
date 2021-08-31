1475e21fbdcd830d7d1c3b38d671ebdb19d86a80
DivyamPal <85020103+DivyamPal@users.noreply.github.com>
2021-08-31 20:45:17 +0530

    Adding files

--- day29/src/store.js
+++ day29/src/store.js
@@ -7,7 +7,7 @@
 
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from "./reducers/index,";
+import rootReducer from "./reducers/index";
 
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
